d443ef4b8cb97264189abe120d04fa9587b68c0e
nbl3cj <nbl3cj@virginia.edu>
2020-02-15 16:26:29 -0500

    edited views.py and added urls.py

--- dash/grades/views.py
+++ dash/grades/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.http import require_POST
+from django.views import generic
 
+from .models import course
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'grades/index.html'
+    context_object_name = 'test'
+
+    def get_queryset(self):
+        return 
