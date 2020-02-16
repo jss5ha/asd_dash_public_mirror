30bc0278067b81546e8957437f13164bac5b678f
nbl3cj <nbl3cj@virginia.edu>
2020-02-15 21:33:11 -0500

    admin.py changes, makemigrations, and bootstrap additions

--- dash/grades/admin.py
+++ dash/grades/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import course
+from .models import course, assignment, assType
 # Register your models here.
 
-admin.site.register(course)
+class AssignmentTypeInLine(admin.TabularInline):
+    model = assType
+    extra = 3
+
+class CourseAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['course_name']}),
+    ]
+    inlines = [AssignmentTypeInLine]
+    # list_display = ('course_name')
+    search_fields = ['course_name']
+admin.site.register(course, CourseAdmin)
