b7c7d1ccc665e4a8c01378f56e9a1070d8e5b867
Rickey Guo <rickeyguo@gmail.com>
2020-03-01 22:04:29 -0500

    attempting to resolve django.db. operationalError on travis

--- dash/settings.py
+++ dash/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import django_heroku
 import os
 
+if 'HEROKU' in os.environ:
+    django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,4 +125,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
