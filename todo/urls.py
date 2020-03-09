74034cd2bc7351a937d6e61d3787a767fca29fc4
Vivian Chen <33379529+vivianchen98@users.noreply.github.com>
2020-03-09 18:54:29 -0400

    deselect added

--- todo/urls.py
+++ todo/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.addTodo, name='add'),
     path('complete/<todo_id>', views.completeTodo, name='complete'),
+    path('deselect/<todo_id>', views.deselect, name='deselect'),
     path('deletecomplete', views.deleteCompleted, name='deletecomplete'),
     path('deleteall', views.deleteAll, name='deleteall')
 ]
