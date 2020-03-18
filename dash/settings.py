7b43fab017e2055bd3993f76530cf0bdeefa2abb
nbl3cj <nbl3cj@virginia.edu>
2020-03-18 17:17:38 -0400

    kjasdgl

--- dash/settings.py
+++ dash/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
-import os
 
+import os
+import django_heroku
 
 '''
 used this tutorial to help set up google oauth
