630937a4cf2e38fe967540f57d95e43258217a8a
Rickey Guo <rickeyguo@gmail.com>
2020-04-01 14:11:07 -0400

    adding whitenoise to make heroku load static

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 release: python manage.py migrate
-python manage.py collectstatic --noinput
 web: gunicorn -- dash.wsgi --log-level debug
--- dash/settings.py
+++ dash/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -155,3 +156,5 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
