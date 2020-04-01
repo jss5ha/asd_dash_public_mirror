05819e612e8986575a7ca685c3cf30c2a4427ea9
Rickey Guo <rickeyguo@gmail.com>
2020-04-01 16:24:36 -0400

    uses django.utils.timezone.now for due date

--- todo/models.py
+++ todo/models.py
@@ -2,12 +2,14 @@ from django.db import models
 import datetime
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from django.utils import timezone
+
 
 class Todo(models.Model):
-    owner = models.ForeignKey(User,null=True, unique=False, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, null=True, unique=False, on_delete=models.CASCADE)
     text = models.CharField(max_length=40)
     complete = models.BooleanField(default=False)
-    due_date = models.DateField(default=datetime.datetime.now())
+    due_date = models.DateField(default=datetime.datetime.now)
 
     def __str__(self):
         return self.text
