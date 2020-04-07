c2b2b24e896246af6e0d1090895e9065fd11de56
nbl3cj <nbl3cj@virginia.edu>
2020-04-06 20:40:30 -0400

    revert

--- dash/settings.py
+++ dash/settings.py
@@ -102,12 +102,8 @@ WSGI_APPLICATION = 'dash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'meme',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
