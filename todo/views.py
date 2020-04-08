f743a579367cb57320300f55da9a87b2ae54f66b
Joe Spaeth <43396274+jss5ha@users.noreply.github.com>
2020-04-08 16:47:49 -0400

    fixed the user stuff

--- todo/views.py
+++ todo/views.py
@@ -7,7 +7,7 @@ from .forms import TodoForm
 
 
 def index(request):
-    todo_list = Todo.objects.order_by('due_date')
+    todo_list = Todo.objects.filter(owner=request.user).order_by('due_date')
     form = TodoForm()
     context = {'todo_list' : todo_list, 'form' : form}
     return render(request, 'todo/index.html', context)
