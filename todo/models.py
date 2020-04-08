f76ff677e8ab7e1caa5547db5b5ab1258f1c69fc
Rickey Guo <rickeyguo@gmail.com>
2020-04-08 16:09:27 -0400

    resolving django datetime warning

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 import datetime
 #
 # GROUP_CHOICES = (
@@ -11,7 +12,7 @@ import datetime
 class Todo(models.Model):
     text = models.CharField(max_length=100)
     complete = models.BooleanField(default=False)
-    due_date = models.DateField(default=datetime.datetime.now())
+    due_date = models.DateField(default=datetime.datetime.now)
     group = models.CharField(max_length=100, default="school")
 
     def __str__(self):
