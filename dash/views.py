9dc2b27646371e118fede4b302deb0d2bba3c685
nbl3cj <nbl3cj@virginia.edu>
2020-04-21 17:15:41 -0400

    finished(?) ui for grades

--- dash/views.py
+++ dash/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html')
+
+class AuthRequiredMiddleware(object):
+    def process_request(self, request):
+        if not request.user.is_authenticated():
+            return HttpResponseRedirect('/') # or http response
+        return None
