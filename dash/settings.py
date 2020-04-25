4e1a1680c1d4f9e6b58ff6854518e493ed03c32a
nbl3cj <nbl3cj@virginia.edu>
2020-04-25 18:37:48 -0400

    whitenoise settings.py changes

--- dash/settings.py
+++ dash/settings.py
@@ -169,7 +169,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 # django_heroku.settings(locals())
